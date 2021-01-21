ecc6a4609eb80a28a039643da3dffca6d7fe7371
lucyMuellenberg23 <lucy.muellenberg@mail.com>
2021-01-21 12:48:09 +0100

    session changes

--- index.js
+++ index.js
@@ -3,15 +3,29 @@ const app = express();
 const User = require('./models/user');
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
-const session = require ('express-session');
+const session = require('express-session');
 
-mongoose.connect('mongodb://localhost:27017/authentication', {useNewUrlParser: true, useUnifiedTopology: true})
+
+
+const sessionConfig = {
+    name: 'session',
+    secret: 'thisisnotagoodsecret',
+    resave: 'false',
+    saveUninitialized: true,
+    cookies: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+}
+
+mongoose.connect('mongodb://localhost:27017/authentication', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log("MONGO CONNECTION OPEN!!")
     })
     .catch(err => {
         console.log("OH NO MONGO CONNECTION ERROR!!")
-    console.log(err)
+        console.log(err)
     })
 
 app.set('view engine', 'ejs');
@@ -19,18 +33,19 @@ app.set('views', 'views');
 
 app.use(express.static('public'))
 
-app.use(express.urlencoded({extended: true}));
-app.use(session({secret:'notagoodsecret'}))
+app.use(express.urlencoded({ extended: true }));
+app.use(session(sessionConfig))
 
-const requireLogin = (req, res, next) => {
-    if (!req.session.user_id) {
-        return res.redirect('/login')
-    }
-    next();
-}
-app.get('/', (req, res) => {
-    res.render('homepage')
-})
+
+// const requireLogin = (req, res, next) => {
+//     if (!req.session.user_id) {
+//         return res.redirect('/login')
+//     }
+//     next();
+// }
+// app.get('/', (req, res) => {
+//     res.render('homepage')
+// })
 
 app.get('/schueler', (req, res) => {
     res.render('schueler')
@@ -41,7 +56,7 @@ app.get('/schulen', (req, res) => {
 })
 
 app.get('/ueber', (req, res) => {
-    res.render('ueber', {title: "ueber"})
+    res.render('ueber', { title: "ueber" })
 })
 
 app.get('/kontakt', (req, res) => {
@@ -73,41 +88,41 @@ app.get('/register', (req, res) => {
 })
 
 app.post('/register', async (req, res) => {
-    const {password, username} = req.body; 
-    const user = new User ({username, password})
+    const { password, username } = req.body;
+    const user = new User({ username, password })
     await user.save();
     res.redirect('/')
 })
 
 app.get('/login', (req, res) => {
-    res.render('login') 
+    res.render('login')
 })
 
-app.post('/login', async  (req, res) => {
-    const {username, password} = req.body;
-    const foundUser = await User.findandValidate(username, password);
-    if(foundUser){
-        req.session.user_id = foundUser._id;
-        res.redirect('/secret');
-    }
-    else {
-        res.redirect('/login')
-    }
-})
+// app.post('/login', async (req, res) => {
+//     const { username, password } = req.body;
+//     const foundUser = await User.findandValidate(username, password);
+//     if (foundUser) {
+//         req.session.user_id = foundUser._id;
+//         res.redirect('/secret');
+//     }
+//     else {
+//         res.redirect('/login')
+//     }
+// })
 
 app.post('/logout', (req, res) => {
     req.session.user_id = null;
-   // req.session.destroy();
+    // req.session.destroy();
     res.redirect('/login');
 })
 
-app.get('/secret', requireLogin, (req, res) => {
-    res.send('secret')
-})
-app.get('/topsecret', requireLogin, (req, res) => {
-    res.send("TOP SECRET!!")
-})
+// app.get('/secret', requireLogin, (req, res) => {
+//     res.send('secret')
+// })
+// app.get('/topsecret', requireLogin, (req, res) => {
+//     res.send("TOP SECRET!!")
+// })
 
 app.listen(3000, () => {
-console.log("SERVING YOUR APP!")
+    console.log("SERVING YOUR APP!")
 })
