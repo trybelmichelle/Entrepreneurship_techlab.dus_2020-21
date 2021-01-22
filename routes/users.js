98f4cc1ea1254140039f7e97e92fdaff909b51fb
lucyMuellenberg23 <lucy.muellenberg@mail.com>
2021-01-22 15:48:52 +0100

    update route users.js

--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,11 @@ router.get('/register', (req, res) => {
 })
 
 router.post('/register', async (req, res) => {
-    res.send(req.body);
+    const { email, username, password } = req.body;
+    const user = new User({ email, username });
+    const registeredUser = await User.register(user, password);
+    // res.redirect('./homepage');
+
 })
 
 
