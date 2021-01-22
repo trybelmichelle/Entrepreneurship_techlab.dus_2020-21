adad6934ff5eb3b42e9129ef79601f46bc5395cd
lucyMuellenberg23 <lucy.muellenberg@mail.com>
2021-01-22 15:38:36 +0100

    passport authentication, usersroute & register

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
-const User = require('./models/user');
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const session = require('express-session');
 
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
 
+const userRoutes = require('./routes/users');
 
 const sessionConfig = {
     name: 'session',
@@ -35,6 +38,17 @@ app.use(express.static('public'))
 
 app.use(express.urlencoded({ extended: true }));
 app.use(session(sessionConfig))
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+
+app.use('/', userRoutes);
 
 
 // const requireLogin = (req, res, next) => {
--- models/user.js
+++ models/user.js
@@ -1,26 +1,29 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const passportLocalMongoose = require('passport-local-mongoose');
+const passport = require('passport');
+const Schema = mongoose.Schema;
 
-const userSchema = new mongoose.Schema({
-    username: {
+const UserSchema = new mongoose.Schema({
+    email: {
         type: String,
-        required: [true,'Username cannot be blank']
-     },
-    password: {
-    type: String,
-    required: [true,'Password cannot be blank']
+        required: [true, 'Username cannot be blank'],
+        unique: true
     }
 })
 
-userSchema.statics.findandValidate = async function (username, password) {
-    const foundUser = await this.findOne({username});
-    const isValid = await bcrypt.compare(password, foundUser.password);
-    return isValid ? foundUser: false;
-}
-userSchema.pre('save', async function(next){
-    if(!this.isModified('password')) return next ();
-    this.password = await bcrypt.hash(this.password, 12);
-next();
-})
+// userSchema.statics.findandValidate = async function (username, password) {
+//     const foundUser = await this.findOne({ username });
+//     const isValid = await bcrypt.compare(password, foundUser.password);
+//     return isValid ? foundUser : false;
+// }
+// userSchema.pre('save', async function (next) {
+//     if (!this.isModified('password')) return next();
+//     this.password = await bcrypt.hash(this.password, 12);
+//     next();
+// })
 
-module.exports = mongoose.model('User', userSchema);
+
+UserSchema.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model('User', UserSchema);
