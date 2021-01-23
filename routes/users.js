058aeef7c565d92dbb07f42744dbcedea99d0834
lucyMuellenberg23 <lucy.muellenberg@mail.com>
2021-01-23 12:55:53 +0100

    lernpfad pfds

--- routes/users.js
+++ routes/users.js
@@ -12,7 +12,7 @@ router.post('/register', async (req, res) => {
     const { email, username, password } = req.body;
     const user = new User({ email, username });
     const registeredUser = await User.register(user, password);
-    // res.redirect('../views/homepage');
+    // res.redirect('./views/homepage');
 })
 
 router.get('/login', (req, res) => {
